054d61ff4844a09e6741e7f93625d6f8ba0164a0
Fernando Bonfim <devnoob@vivaldi.net>
2023-02-02 20:21:12 -0300

    Proper type of imported PNG added

--- src/@types/png.d.ts
+++ src/@types/png.d.ts
@@ -1,1 +1,12 @@
-declare module '*.png';
+// Tell TS to import all PNG as modules *1
+declare module '*.png' {
+    const content: ImageSourcePropType;
+    export default content;
+}
+
+
+/*
+Dev's comments
+1 - A module is a self-contained unit of code that exports some values, which can then be imported and used in another part of your code. Treating a .png file as a module, means 
+*/
+
