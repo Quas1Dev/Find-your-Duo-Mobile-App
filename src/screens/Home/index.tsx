f29c4d1a7a78381cd8d8ea58ec36aa4283855a0e
Fernando Bonfim <devnoob@vivaldi.net>
2023-02-02 21:45:43 -0300

    Full homt component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, FlatList } from 'react-native';
+import logo from '../../assets/logo-nlw-esports.png';
+import { GameCard } from '../../components/GameCard';
+import { Heading } from '../../components/Heading';
+import { styles } from './styles';
+import { GAMES } from '../../utils/games';
 
-export default function Home () {
+export default function Home() {
     return (
-        <View>
-        <Text>
-            I'm in home.
-        </Text>
+        <View style={styles.container}>
+            <Image source={logo} style={styles.logo} />
+            <Heading
+                title="Encontre seu duo"
+                subtitle="Selecione o game que deseja jogar..."
+            />
+            <FlatList
+                data={GAMES}
+                keyExtractor={item => item.id}
+                renderItem={({item}) => <GameCard data={item}/>}
+                showsHorizontalScrollIndicator={false}
+                horizontal
+            />
         </View>
     )
 }
