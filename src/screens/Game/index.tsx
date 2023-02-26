8b75a0d32f1c418e3d7f010997e9ab1c04b8cad3
Fernando Bonfim <devnoob@vivaldi.net>
2023-02-26 19:15:06 -0300

    Added new screen Game

---
+++ src/screens/Game/index.tsx
@@ -0,0 +1,11 @@
+import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { styles } from './styles';
+
+export default function Game() {
+  return (
+    <SafeAreaView style={styles.container}>
+
+    </SafeAreaView>
+  );
+}
