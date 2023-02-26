1a9f3dbc015c85920aed5dc6f51b7dc21eff5ba8
Fernando Bonfim <devnoob@vivaldi.net>
2023-02-26 16:16:16 -0300

    Consuming API's games

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, Image, FlatList, Button } from 'react-native';
 import logo from '../../assets/logo-nlw-esports.png';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
 import { styles } from './styles';
 import { GAMES } from '../../utils/games';
+import api from '../../AxiosConfig/AxiosConfig';
 
 export default function Home() {
+    const [games, setGames] = useState<GameCardProps[]>([])
+    useEffect(() => {
+        async function fetchGames() {
+            try {
+                const response = await api.get<GameCardProps[]>('/games')
+                console.log(response.data)
+                setGames(response.data);
+            } catch (err: any) {
+                console.error("Error:", err.message);
+                throw new Error("Something went wrong!");
+            }
+        }
+        fetchGames()
+    }, [])
+    console.log(games)
     return (
         // Our screen main container
         <View style={styles.container}>
@@ -19,9 +35,9 @@ export default function Home() {
             />
             {/* Ads list */}
             <FlatList
-                data={GAMES}
+                data={games}
                 keyExtractor={item => item.id}
-                renderItem={({item}) => <GameCard data={item}/>}
+                renderItem={({ item }) => <GameCard data={item} />}
                 showsHorizontalScrollIndicator={false}
                 horizontal
                 contentContainerStyle={styles.contentList}
