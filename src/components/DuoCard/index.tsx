ee56088736f5b80f831f264ce5e7ff48cf03ac70
Fernando Bonfim <devnoob@vivaldi.net>
2023-03-19 13:42:24 -0300

    Added basic set of label/values set

--- src/components/DuoCard/index.tsx
+++ src/components/DuoCard/index.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
 import { View } from 'react-native';
+import { DuoInfo } from '../DuoInfo';
 
 import { styles } from './styles';
 
 export function DuoCard() {
   return (
     <View style={styles.container}>
+      <DuoInfo
+        label="Nome"
+        value=""
+      />
 
+      <DuoInfo
+        label="Tempo de Jogo"
+        value=""
+      />
+
+      <DuoInfo
+        label="Disponibilidade"
+        value=""
+      />
+
+      <DuoInfo
+        label="Chamada de áudio"
+        value=""
+      />
     </View>
   );
 }
