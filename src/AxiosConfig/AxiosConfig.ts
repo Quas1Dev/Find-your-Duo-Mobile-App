b3e1e316d0348337728ac2a4725ed1f5682da789
Fernando Bonfim <devnoob@vivaldi.net>
2023-03-19 11:08:56 -0300

    Changed port to connect to

--- src/AxiosConfig/AxiosConfig.ts
+++ src/AxiosConfig/AxiosConfig.ts
@@ -1,7 +1,16 @@
 import axios, { AxiosInstance } from 'axios'
 
 const api : AxiosInstance = axios.create({
-    baseURL: 'http://192.168.15.7:3333'
+    baseURL: 'http://192.168.15.8:3333'
 })
 
 export default api;
+
+/*
+Dev's comments
+
+Sometimes the local IP for the computer holding the server changes.
+Use the ipconfig command in the CMD to find out the local IP address.
+Look under "Adaptador de Rede sem Fio Wi-Fi:" and copy the sequence 
+corresponding to "Endereço IPv4".
+*/
