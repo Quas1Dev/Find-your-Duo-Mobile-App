00149710e222006ec5798ff77459764fccc6f4aa
Fernando Bonfim <devnoob@vivaldi.net>
2023-03-19 11:21:01 -0300

    Using params

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,9 +1,13 @@
-import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useRoute } from '@react-navigation/native';
 import { Background } from '../../components/background';
 import { styles } from './styles';
+import { GameParams } from '../../@types/navigation';
 
 export default function Game() {
+  const route = useRoute();
+  const game = route.params as GameParams;
+ 
   return (
     <Background>
       <SafeAreaView style={styles.container}>
