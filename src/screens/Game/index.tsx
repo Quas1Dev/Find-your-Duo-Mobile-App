57ab3c3b177dad67dd8ec789b2c3063750f2155d
Fernando Bonfim <devnoob@vivaldi.net>
2023-03-20 18:22:54 -0300

    Added flat list; handled goBack button;

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -3,7 +3,7 @@ import { useRoute, useNavigation } from '@react-navigation/native';
 import { Background } from '../../components/background';
 import { styles } from './styles';
 import { GameParams } from '../../@types/navigation';
-import { FlatList, Image, TouchableOpacity, View } from 'react-native';
+import { FlatList, Image, TouchableOpacity, View, Text } from 'react-native';
 import logoImg from "../../assets/logo-nlw-esports.png";
 import { Entypo } from "@expo/vector-icons"
 import { THEME } from '../../theme';
@@ -27,7 +27,7 @@ export interface AdsInfo {
 
 export default function Game() {
   const route = useRoute();
-  const [ads, setAds] = useState<AdsInfo[]>();
+  const [ads, setAds] = useState<AdsInfo[]>([]);
   const game = route.params as GameParams;
   const navigator = useNavigation();
 
@@ -40,7 +40,6 @@ export default function Game() {
       try {
         const result = await api.get<AdsInfo[]>(`games/${game.id}/ads`)
         setAds(result.data);
-        console.log("result:", result.data[0].hourend);
       } catch (err: any) {
         console.log(err.message)
         console.log("Something went wrong!")
@@ -54,7 +53,7 @@ export default function Game() {
     <Background>
       <SafeAreaView style={styles.container}>
         <View style={styles.header}>
-          <TouchableOpacity onPress={() => { handleGoBack() }}>
+          <TouchableOpacity onPress={ handleGoBack }>
             <Entypo
               name="chevron-thin-left"
               color={THEME.COLORS.CAPTION_300}
@@ -81,11 +80,21 @@ export default function Game() {
         <Heading
           title={game.title}
           subtitle='Conecte-se e comece a jogar.' />
-        {ads && <FlatList
+
+        <FlatList
           data={ads}
           keyExtractor={item => item.id}
-          renderItem = {({item})=><DuoCard data={item}/>}
-        />}
+          renderItem={({ item }) => <DuoCard data={item} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={[ads.length > 0 ? styles.contentList : styles.emptyListContent]}
+          style={styles.containerList}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              Não há anúncios publicados ainda.
+            </Text>
+          )}
+        />
       </SafeAreaView>
     </Background>
   );
