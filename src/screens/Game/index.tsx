90a9aaf44e4bf72266e31e31f812fe4c91d352d0
Fernando Bonfim <devnoob@vivaldi.net>
2023-03-20 17:03:34 -0300

    Defined ads type; loaded ads; displayed ads list

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -3,21 +3,52 @@ import { useRoute, useNavigation } from '@react-navigation/native';
 import { Background } from '../../components/background';
 import { styles } from './styles';
 import { GameParams } from '../../@types/navigation';
-import { Image, TouchableOpacity, View } from 'react-native';
+import { FlatList, Image, TouchableOpacity, View } from 'react-native';
 import logoImg from "../../assets/logo-nlw-esports.png";
 import { Entypo } from "@expo/vector-icons"
 import { THEME } from '../../theme';
 import { Heading } from '../../components/Heading';
 import { DuoCard } from '../../components/DuoCard';
+import { useEffect, useState } from 'react';
+import api from '../../AxiosConfig/AxiosConfig';
+
+export interface AdsInfo {
+  id: string;
+  gameid: string;
+  name: string;
+  yearsplaying: Number;
+  discord: string;
+  weekdays: Array<number>;
+  hourstart: number;
+  hourend: string;
+  usevoicechannel: boolean;
+  createdate: string;
+}
 
 export default function Game() {
   const route = useRoute();
+  const [ads, setAds] = useState<AdsInfo[]>();
   const game = route.params as GameParams;
   const navigator = useNavigation();
 
   function handleGoBack() {
     navigator.goBack();
   }
+
+  useEffect(() => {
+    async function findAdsByGameId() {
+      try {
+        const result = await api.get<AdsInfo[]>(`games/${game.id}/ads`)
+        setAds(result.data);
+        console.log("result:", result.data[0].hourend);
+      } catch (err: any) {
+        console.log(err.message)
+        console.log("Something went wrong!")
+      }
+    }
+
+    findAdsByGameId();
+  }, [])
 
   return (
     <Background>
@@ -46,11 +77,15 @@ export default function Game() {
           style={styles.cover}
           resizeMode='cover'
         />
-        
+
         <Heading
           title={game.title}
           subtitle='Conecte-se e comece a jogar.' />
-        <DuoCard />
+        {ads && <FlatList
+          data={ads}
+          keyExtractor={item => item.id}
+          renderItem = {({item})=><DuoCard data={item}/>}
+        />}
       </SafeAreaView>
     </Background>
   );
