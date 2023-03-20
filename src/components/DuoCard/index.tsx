5f58feb82b7b08e67c732b7399c2fb69e2c3c14b
Fernando Bonfim <devnoob@vivaldi.net>
2023-03-20 18:25:24 -0300

    Finished DuoCard component

--- src/components/DuoCard/index.tsx
+++ src/components/DuoCard/index.tsx
@@ -1,7 +1,8 @@
-import { View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { AdsInfo } from '../../screens/Game';
+import { THEME } from '../../theme';
 import { DuoInfo } from '../DuoInfo';
-
+import { Ionicons } from '@expo/vector-icons';
 import { styles } from './styles';
 interface Props {
   data: AdsInfo;
@@ -23,14 +24,21 @@ export function DuoCard({ data }: Props) {
 
       <DuoInfo
         label="Disponibilidade"
-        value={`${data.weekdays.length} dias`}
+        value={`${data.weekdays.length} dias \u2022 ${data.hourstart} - ${data.hourend}`}
       />
 
       <DuoInfo
         label='Chamada de áudio?'
-        value={data.usevoicechannel? "Sim" : "Não"}
+        value={data.usevoicechannel ? "Sim" : "Não"}
+        color={data.usevoicechannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT}
       />
 
+      <TouchableOpacity style={styles.button}>
+        <Ionicons name="ios-game-controller-outline" size={20} color={THEME.COLORS.TEXT}/>
+        <Text style={styles.buttonTitle}>
+          Conectar
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
