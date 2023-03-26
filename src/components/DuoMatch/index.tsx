a6cd187789028e8ea0c78739fba478590ad5f67b
Fernando Bonfim <devnoob@vivaldi.net>
2023-03-26 17:58:17 -0300

    Finished Modal component structure

--- src/components/DuoMatch/index.tsx
+++ src/components/DuoMatch/index.tsx
@@ -1,18 +1,50 @@
-import { View, Modal, ModalProps, Text } from 'react-native';
-
+import { View, Modal, ModalProps, Text, TouchableOpacity } from 'react-native';
+import { MaterialIcons, AntDesign } from "@expo/vector-icons";
 import { styles } from './styles';
+import { THEME } from '../../theme';
+import { Heading } from '../Heading';
 
 interface Props extends ModalProps {
   discord: string;
+  onClose: () => void
 }
 
-export function DuoMatch({discord, ...rest} : Props) {
+export function DuoMatch({ discord, onClose, ...rest }: Props) {
   return (
-    <Modal {...rest}>
+    <Modal
+      animationType='fade'
+      transparent
+      statusBarTranslucent
+      {...rest}>
       <View style={styles.container}>
-        <Text style={styles.discord}>
-         
-        </Text>
+        <View style={styles.content}>
+          <TouchableOpacity
+            style={styles.closeIcons}
+            onPress={onClose}
+          >
+            <MaterialIcons
+              name="close"
+              size={20}
+              color={THEME.COLORS.CAPTION_500}
+            />
+          </TouchableOpacity>
+          <AntDesign name="checkcircleo" size={64} color={THEME.COLORS.SUCCESS} style={{ fontWeight: 'bold' }} />
+
+          <Heading
+            title="Let's play"
+            subtitle='Agora é só começar a jogar!'
+            style={{ alignItems: 'center', marginTop: 24 }}
+          />
+
+          <Text style={styles.label}>
+            Adicione seu discord.
+          </Text>
+          <TouchableOpacity style={styles.discordBtn}>
+            <Text style={styles.discord}>
+              {discord}
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </Modal>
   );
