22880b0711aa3c322a850557c21aee24c9ac7ae1
Fernando Bonfim <devnoob@vivaldi.net>
2023-03-26 16:27:15 -0300

    Modal set

---
+++ src/components/DuoMatch/index.tsx
@@ -0,0 +1,19 @@
+import { View, Modal, ModalProps, Text } from 'react-native';
+
+import { styles } from './styles';
+
+interface Props extends ModalProps {
+  discord: string;
+}
+
+export function DuoMatch({discord, ...rest} : Props) {
+  return (
+    <Modal {...rest}>
+      <View style={styles.container}>
+        <Text style={styles.discord}>
+         
+        </Text>
+      </View>
+    </Modal>
+  );
+}
